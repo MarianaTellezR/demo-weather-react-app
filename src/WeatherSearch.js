ab8a868697eff7f8eecf68593e0f0ea9ac679243
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-07-21 13:46:41 -0600

    Created Search Form

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -1,5 +1,23 @@
 import React from "react";
+import "./WeatherSearch.css";
 
 export default function WeatherSearch() {
-    return(<h1>Hello</h1>)
+  return (
+    <div className="WeatherSearch">
+      <div className="get-current-info">
+        <form>
+          <input
+            type="search"
+            placeholder="Enter a city"
+            autoComplete="off"
+            className="input-search"
+          />
+          <button type="submit" className="btn btn-primary search">
+            Search
+          </button>
+        </form>
+        <button className="btn btn-success current">Current</button>
+      </div>
+    </div>
+  );
 }
