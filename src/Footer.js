052a0fa85482c8aa9bdffaa7a6ab9c8d2a46f029
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-07-21 15:10:25 -0600

    Updated footer and title

---
+++ src/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <p>
+        <a
+          href="https://github.com/MarianaTellezR/demo-weather-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open source code
+        </a>
+        , by{" "}
+        <a
+          href="www.linkedin.com/in/mariana-tellez-resendiz-134354232"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Mariana Tellez Resendiz
+        </a>
+      </p>
+    </div>
+  );
+}
